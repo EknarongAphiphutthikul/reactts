be17b90bbd3d44f93b74ae724e1475ea3b461e1e
Eknarong Aphiphutthikul <eknarong.aph@ascendcorp.com>
2025-01-06 10:52:09 +0700

    init upload-file-api

---
+++ poc-upload-file/spring-upload-file-api/src/main/kotlin/org/example/springuploadfileapi/SpringUploadFileApiApplication.kt
@@ -0,0 +1,11 @@
+package org.example.springuploadfileapi
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class SpringUploadFileApiApplication
+
+fun main(args: Array<String>) {
+    runApplication<SpringUploadFileApiApplication>(*args)
+}
---
+++ poc-upload-file/spring-upload-file-api/src/test/kotlin/org/example/springuploadfileapi/SpringUploadFileApiApplicationTests.kt
@@ -0,0 +1,13 @@
+package org.example.springuploadfileapi
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class SpringUploadFileApiApplicationTests {
+
+    @Test
+    fun contextLoads() {
+    }
+
+}
